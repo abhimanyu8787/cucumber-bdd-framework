9249dfd4bb0d377a5de845404e5e2020ae3d0b39
Abhimanyu <abhimanyukumar8787@gmail.com>
2023-03-04 13:38:25 +0530

    Added Get Methods to get GlobalsqaDemo Site Pages

--- src/test/java/bdd/automation/utils/PageObjectManager.java
+++ src/test/java/bdd/automation/utils/PageObjectManager.java
@@ -3,6 +3,8 @@ package bdd.automation.utils;
 import org.openqa.selenium.WebDriver;
 
 import bdd.automation.pages.DashboardPage;
+import bdd.automation.pages.GlobalsqaDemoSite;
+import bdd.automation.pages.GlobalsqaDemoSiteDragAndDrop;
 import bdd.automation.pages.LoginPage;
 import bdd.automation.pages.PersonalInformationManagementPage;
 
@@ -12,6 +14,8 @@ public class PageObjectManager {
     private LoginPage loginPage;
     private DashboardPage dashboardPage;
     private PersonalInformationManagementPage personalManagementPage;
+    private GlobalsqaDemoSite globalsqaDemoSite;
+    private GlobalsqaDemoSiteDragAndDrop globalsqaDemoSiteDragAndDrop;
     
     public PageObjectManager(WebDriver driver) {
         this.driver = driver;
@@ -30,6 +34,16 @@ public class PageObjectManager {
     public PersonalInformationManagementPage getPersonalInformationManagementPage() {
         personalManagementPage = new PersonalInformationManagementPage(driver);
         return personalManagementPage;
+    }
+    
+    public GlobalsqaDemoSite getGlobalsqaDemoSite() {
+        globalsqaDemoSite = new GlobalsqaDemoSite(driver);
+        return globalsqaDemoSite;
+    }
+    
+    public GlobalsqaDemoSiteDragAndDrop getGlobalsqaDemoSiteDragAndDrop() {
+        globalsqaDemoSiteDragAndDrop = new GlobalsqaDemoSiteDragAndDrop(driver);
+        return globalsqaDemoSiteDragAndDrop;
     }
 
 }
